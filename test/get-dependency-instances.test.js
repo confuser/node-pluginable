9efdad0a894ec86b6c4833c1712e88a9b71fd72d
James Mortemore <james.mortemore@carthrottle.com>
2015-11-02 15:35:02 +0000

    Fixed test

--- test/get-dependency-instances.test.js
+++ test/get-dependency-instances.test.js
@@ -1,21 +1,16 @@
-var createServiceLocator = require('service-locator')
-  , assert = require('assert')
+var assert = require('assert')
   , getDependencyInstances = require('../lib/get-dependency-instances')
 
 describe('Get Dependency Instances', function () {
   it('should return an empty array', function () {
-    var deps = getDependencyInstances(createServiceLocator(), {})
+    var deps = getDependencyInstances({}, {})
 
     assert.equal(deps.length, 0)
   })
 
   it('should return dependency instances', function () {
-    var serviceLocator = createServiceLocator()
-
-    serviceLocator.register('test', 'hello')
-    serviceLocator.register('test2', 'foobar')
-
-    var deps = getDependencyInstances(serviceLocator, { dependencies: [ 'test', 'test2' ] })
+    var serviceLocator = { test: 'hello', test2: 'foobar' }
+      , deps = getDependencyInstances(serviceLocator, { dependencies: [ 'test', 'test2' ] })
 
     assert.equal(deps.length, 2)
     assert.equal(deps[0], 'hello')
